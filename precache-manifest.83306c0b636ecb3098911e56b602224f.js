088de2335c331c84d1dc07f2f910e3321036d7f6
sughosh-mages <52438764+sughosh-mages@users.noreply.github.com>
2020-11-06 21:09:08 +0530

    Add files via upload

---
+++ precache-manifest.83306c0b636ecb3098911e56b602224f.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3047b68c998d89871eb7c630729bec2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0502cbf236b28b7bc318",
+    "url": "/static/css/main.a3f2f618.chunk.css"
+  },
+  {
+    "revision": "f9cbebd8e21243ae124b",
+    "url": "/static/js/2.686aad58.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.686aad58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0502cbf236b28b7bc318",
+    "url": "/static/js/main.39acc951.chunk.js"
+  },
+  {
+    "revision": "bf6c79284ce4adc94239",
+    "url": "/static/js/runtime-main.0d74ac24.js"
+  },
+  {
+    "revision": "3d618af61eb6ef68c6a0b39f12e82ac1",
+    "url": "/static/media/3DBox.3d618af6.jpg"
+  },
+  {
+    "revision": "dcadf6814e722b43429527887c9951c6",
+    "url": "/static/media/Analytics.dcadf681.png"
+  },
+  {
+    "revision": "74352860a6081c346488cb0f5f643196",
+    "url": "/static/media/Brochure.74352860.jpg"
+  },
+  {
+    "revision": "82211ea98c32517c1a704f3297f24a8c",
+    "url": "/static/media/BuisnessCard.82211ea9.jpg"
+  },
+  {
+    "revision": "e6ab954b98928e75b2cd97740b72b184",
+    "url": "/static/media/Catalog.e6ab954b.jpg"
+  },
+  {
+    "revision": "9d6d5e1e78d491258ee773d176a49c48",
+    "url": "/static/media/Commerce.9d6d5e1e.jpg"
+  },
+  {
+    "revision": "7c327ee141c7d2719ee15a8c0eee8f00",
+    "url": "/static/media/Education.7c327ee1.jpg"
+  },
+  {
+    "revision": "27fcb22c9806ead7b5d5a4b2c1ec1968",
+    "url": "/static/media/EducationFlashCard.27fcb22c.jpg"
+  },
+  {
+    "revision": "1ee82afa05e8e2df5e047faef7bdac32",
+    "url": "/static/media/Financial.1ee82afa.jpg"
+  },
+  {
+    "revision": "454f982abaec6ccfd9e880b459194437",
+    "url": "/static/media/Gallery.454f982a.jpg"
+  },
+  {
+    "revision": "625747396420d897853b7767647e8172",
+    "url": "/static/media/MagesLogo.62574739.png"
+  },
+  {
+    "revision": "a765ee298419f507307b55792ee1f62d",
+    "url": "/static/media/MercedesLogo.a765ee29.png"
+  },
+  {
+    "revision": "b44f54fafacfb1b5f6d93d6c77538953",
+    "url": "/static/media/Poster.b44f54fa.jpg"
+  },
+  {
+    "revision": "2c7dae64dde58046220516eada908d8e",
+    "url": "/static/media/ProductDisplay.2c7dae64.jpg"
+  },
+  {
+    "revision": "0f2f690bb8a434852db63f750a1c2a5c",
+    "url": "/static/media/Tourism.0f2f690b.jpg"
+  },
+  {
+    "revision": "41d4c7f596808341290d095186865972",
+    "url": "/static/media/Transport.41d4c7f5.jpg"
+  },
+  {
+    "revision": "34d1e49a42106c259e8ff6b38bf30fb1",
+    "url": "/static/media/Utilities.34d1e49a.png"
+  },
+  {
+    "revision": "e31ffe3a965d0795d61734f7fc422324",
+    "url": "/static/media/VideoPlayer.e31ffe3a.jpg"
+  },
+  {
+    "revision": "b4a6517a8365e8c0fd0d55d461ce5ab9",
+    "url": "/static/media/dp.b4a6517a.jfif"
+  },
+  {
+    "revision": "3c8619092cb6090deb0277fd81e9a87e",
+    "url": "/static/media/mercedesbenz.3c861909.jpg"
+  },
+  {
+    "revision": "85ed27eddd38e696e8f6a398530b920e",
+    "url": "/static/media/mercedesbenz2.85ed27ed.jpg"
+  },
+  {
+    "revision": "9752573de54ef0f9e5d3b3b5b00356fa",
+    "url": "/static/media/mercedesbenz3.9752573d.png"
+  },
+  {
+    "revision": "4bf044ca84d8348c11a515e6416b4a24",
+    "url": "/static/media/profilepngicon.4bf044ca.jpg"
+  },
+  {
+    "revision": "2d53c3c66201e8c177ce59c400b238b5",
+    "url": "/static/media/reality_img.2d53c3c6.png"
+  }
+]);
